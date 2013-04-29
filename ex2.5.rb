2e99917aa9f7ca64518bd4f3e89fbb072d0cd98b
Jim Coyle <jim.coyle@sage.com>
2013-04-29 17:38:05 +0100

    doing exercises

---
+++ ex2.5.rb
@@ -0,0 +1,10 @@
+# how many hours in a year?
+
+puts 'how many hours in a year'
+puts (365 * 24)
+
+puts 'how many minutes in a decade'
+puts (10 * 365 * 24 * 60)
+
+puts 'my age in seconds'
+puts (48 * 365 * 24 * 60 * 60)
