06c74713c39b8d617a433a2d8aa9d513c48e60b6
Jim Coyle <jim.coyle@sage.com>
2013-05-01 16:55:25 +0100

    new

---
+++ gets.rb
@@ -0,0 +1,4 @@
+# puts gets
+puts 'What\'s your name?'
+my_name = gets.chomp
+puts 'Hello, ' + my_name + ',' + ' nice to meet you.'
---
+++ variable.rb
@@ -0,0 +1,7 @@
+# jims_string = 'Mary had a little lamb '
+# puts jims_string * 5
+
+jims_name = 'James Patrick Francis Coyle'
+puts 'My name is ' + jims_name  + '.'
+puts 'Wow, ' + jims_name
+puts 'is a really nice name.'
